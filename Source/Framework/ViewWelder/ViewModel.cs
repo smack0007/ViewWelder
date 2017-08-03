4aaab7ee9b779a578c29c5735591e4bd942f215d
smack0007 <zachary.snow@gmail.com>
2017-08-03 08:48:20 +0200

    ViewModel now implements IDataErrorInfo.

--- Source/Framework/ViewWelder/ViewModel.cs
+++ Source/Framework/ViewWelder/ViewModel.cs
@@ -1,18 +1,50 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ViewWelder
 {
-    public abstract class ViewModel : INotifyPropertyChanged
+    public abstract class ViewModel : INotifyPropertyChanged, IDataErrorInfo
     {
+        private readonly Dictionary<string, string> propertyErrors = new Dictionary<string, string>();
+
         internal ViewContext ViewContext { get; set; }
 
         public IDialogPresenter DialogPresenter => this.ViewContext;
 
+        string IDataErrorInfo.Error => null;
+
+        string IDataErrorInfo.this[string columnName] => this.GetPropertyError(columnName);
+
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public void NotifyOfPropertyChange(string propertyName)
+        protected void NotifyOfPropertyChange(string propertyName)
         {
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void ClearPropertyErrors()
+        {
+            this.propertyErrors.Clear();
+        }
+
+        protected string GetPropertyError(string propertyName)
+        {
+            if (this.propertyErrors.TryGetValue(propertyName, out var error))
+            {
+                return error;
+            }
+
+            return null;
+        }
+
+        protected void RemovePropertyError(string propertyName)
+        {
+            this.propertyErrors.Remove(propertyName);
+        }
+
+        protected void SetPropertyError(string propertyName, string error)
+        {
+            this.propertyErrors[propertyName] = error;
         }
     }
 }
